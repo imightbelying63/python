6f0f0f2d79ec74d5bb02f3051d538d09b0497505
imightbelying.63 <20290361+imightbelying63@users.noreply.github.com>
2018-04-07 10:33:00 -0400

    readonlyFS

---
+++ text/readOnlyFS.py
@@ -0,0 +1,20 @@
+if __name__ == "__main__":
+    import os
+
+def readOnlyFS():
+    filesystems = ['/etc',
+            '/var',
+            '/var/lib/rpm',
+            '/var/cpanel',
+            '/usr/local',
+            '/usr/local/cpanel',
+            '/usr/local/bin',
+            '/usr/bin',
+            '/tmp',
+            '/var/tmp']
+    for fs in filesystems:
+        if not os.access(fs, os.W_OK|os.X_OX):
+            return False
+    return True
+
+readOnlyFS()
