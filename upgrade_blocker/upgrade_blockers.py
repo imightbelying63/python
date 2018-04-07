35999423b785cb3a4167260815475124f9d53176
imightbelying63 <khughes@liquidweb.com>
2018-04-06 22:39:33 -0400

    adding functionality

--- upgrade_blocker/upgrade_blockers.py
+++ upgrade_blocker/upgrade_blockers.py
@@ -1,6 +1,6 @@
 """This script will check if a cPanel update is available, and if so, are there any blockers that will prevent it from proceeding.
 
-  TODO: ensure run as root
+  TODO: ensure run as root (DONE)
   TODO: verify license file
   TODO: check write-access to required file systems
   TODO: ensure rpm integrity
@@ -16,6 +16,7 @@
 """
 
 import os, sys
+import requests, re
 
 #This script must be run as root
 if os.geteuid() > 0:
@@ -25,3 +26,15 @@ if os.geteuid() > 0:
 with open("/usr/local/cpanel/version") as version_file:
     cpanel_version = version_file.read()[:5]
 print(cpanel_version)
+
+def findReleaseVersion():
+    tier_request = requests.get("http://httpupdate.cpanel.net/cpanelsync/TIERS")
+    if tier_request.status_code == 200:
+        release_regex = re.compile("^release:(.*)?")
+        tier_returns = tier_request.text.splitlines()
+        for tier in tier_returns:
+            if release_regex.match(tier):
+                release_tier_version = release_regex.match(tier).group(1)
+                return release_tier_version
+    elif:
+        return print("Unable to download cpanel tiers")
