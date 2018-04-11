eb26536e6286c6817a939530c973b852e0055d1b
imightbelying63 <user.email=20290361+imightbelying63@users.noreply.github.com>
2018-04-10 20:39:20 -0400

    added readOnlyFS()

--- upgrade_blockers/upgrade_blockers.py
+++ upgrade_blockers/upgrade_blockers.py
@@ -36,6 +36,22 @@ def licenseCheck():
     else:
         return False
 
+def readOnlyFS():
+    filesystems = ['/etc',
+            '/var',
+            '/var/lib/rpm',
+            '/var/cpanel',
+            '/usr/local',
+            '/usr/local/cpanel',
+            '/usr/local/bin',
+            '/usr/bin',
+            '/tmp',
+            '/var/tmp']
+    for fs in filesystems:
+        if not os.access(fs, os.W_OK|os.X_OK):
+            return False
+    return True
+
 #platformDepsCheck()
 #cpanel_version = findCpanelVersion()
 #cpanel_release_version = findReleaseVersion()
