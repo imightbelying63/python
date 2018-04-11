803df120525e26f5fef92849b387d4e946e60add
imightbelying63 <user.email=20290361+imightbelying63@users.noreply.github.com>
2018-04-10 23:47:07 -0400

    improved version reporting; can convert to float

--- upgrade_blockers/tests/mysqlVersion.py
+++ upgrade_blockers/tests/mysqlVersion.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
 
 def mysqlVersion():
     version = subprocess.getstatusoutput('mysqladmin version|grep -i "server version"')[1].expandtabs().split()[2]
-    return ''.join(version[:3])
+    return float(''.join(version[:3]))
 
 if __name__ == "__main__":
     print(mysqlVersion())
