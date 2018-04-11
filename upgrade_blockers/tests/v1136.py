d3ffa2f0a3a9a43d0550a52e38461a8abcff8b41
imightbelying63 <user.email=20290361+imightbelying63@users.noreply.github.com>
2018-04-11 16:22:30 -0400

    updated v1136.py

--- upgrade_blockers/tests/v1136.py
+++ upgrade_blockers/tests/v1136.py
@@ -1,6 +1,6 @@
 """tests for upgrading to 11.36:
    + >= 1.6GB available in /usr/local/cpanel
-   + /usr/local/cpanel/scripts/sysup is disabled
+   + /usr/local/cpanel/scripts/sysup is disabled (SYSUP is never in cpupdate)
    + any of the following are not automatic or inherit in /etc/cpupdate.conf:
      * mysql
      * courier
@@ -17,7 +17,14 @@
 import os,re
 
 def v1136():
-    pass
+    v1136_specific = []
+
+    #disk space
+
+    stat = os.statvfs('/usr/local/cpanel')
+    free = (stat.f_bfree*stat.f_bsize) / (1024**3) #converts to GB
+    if free < 1.6:
+        v1136_specific.append("Insufficient space under /usr/local/cpanel. " + free + "GB available, 1.6GB required")
 
 if __name__ == "__main__":
     print(v1136())
