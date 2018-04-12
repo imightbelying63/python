490f226d9cb0986022033c6e9d954686b2dd2fd1
imightbelying63 <user.email=20290361+imightbelying63@users.noreply.github.com>
2018-04-11 23:13:08 -0400

    complete v1144

--- upgrade_blockers/tests/v1144.py
+++ upgrade_blockers/tests/v1144.py
@@ -5,4 +5,13 @@
 import subprocess
 
 def v1144():
+    v1144_specific = []
 
+    #test for whmxfer
+    if subprocess.getstatusoutput('mysql -Bse "show databases"|grep whmxfer')[0] == 0:
+        v1144_specific.append("The whmxfer must be deleted")
+
+    return v1144_specific
+
+if __name__ == "__main__":
+    print(v1144())
