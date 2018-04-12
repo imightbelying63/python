4690a037e70c305f168cadcc5a67860635def36f
imightbelying63 <user.email=20290361+imightbelying63@users.noreply.github.com>
2018-04-12 15:34:02 -0400

    complete v1146

--- upgrade_blockers/tests/v1146.py
+++ upgrade_blockers/tests/v1146.py
@@ -5,10 +5,26 @@
      /var/cpanel/rpm.versions.d/local.versions
 """
 
-import os
+import os,re
 
 def v1146():
-    pass
+    v1146_specific = []
+
+    #FP extensions
+    if os.path.exists('/usr/local/frontpage/version5.0/bin/owsadm.exe'):
+        v1146_specific.append("FrontPage extensions must be removed.  Use Home >> Front Page >> Uninstall FrontPage Extensions")
+
+    #RPM target
+    rpm_versions_file = '/var/cpanel/rpm.versions.d/local.versions'
+    with open(rpm_versions_file) as file:
+        for line in file.readlines():
+            if re.search('cpanel-php53:\s+(installed)', line):
+                v1146_specific.append('cpanel-php53 RPM target set to installed in ' + rpm_versions_file + """.  Remove it with:
+  /scripts/update_local_rpm_versions --del target_settings.cpanel-php53
+  /scripts/check_cpanel_rpms --fix""")
+                #v1146_specific.append('cpanel-php53 RPM target set to installed in ' + rpm_versions_file + '.  Remove it with /scripts/update_local_rpm_versions --del target_settings.cpanel-php53 and then /scripts/check_cpanel_rpms --fix')
+
+    return v1146_specific
 
 if __name__ == "__main__":
     print(v1146())
