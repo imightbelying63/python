e7d8820ee2b6d0f7cd7ac2ecf51eb59e14c68e26
imightbelying63 <user.email=20290361+imightbelying63@users.noreply.github.com>
2018-04-12 20:43:49 -0400

    init v1160

---
+++ upgrade_blockers/tests/v1160.py
@@ -0,0 +1,12 @@
+"""tests for 1160:
+   + SNI webserver Apache (httpd) 2.2.12, Litespeed 4.1, or Openlitespeed 1.4.12 or greater
+    * this will only handle apache, our lsws installs are almost always 5.0 anyway
+"""
+
+import subprocess
+
+def v1160():
+    pass
+
+if __name__ == "__main__":
+    print(v1160())
