48976b220a226c87ba097ee4cad8b7bf92979339
imightbelying63 <user.email=20290361+imightbelying63@users.noreply.github.com>
2018-04-12 22:29:16 -0400

    init v1168

---
+++ upgrade_blockers/tests/v1168.py
@@ -0,0 +1,12 @@
+"""tests for upgrading to 68:
+   + lsws 5.2.1 build 2 or later
+   + if EA4, ea-apache24-config-runtime >= 1.0-113
+"""
+
+import subprocess
+
+def v1168():
+    pass
+
+if __name__ == "__main__":
+    print(v1168())
