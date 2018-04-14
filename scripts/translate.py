09443b188795a90df3827d3fd595fc75bc0f1711
imightbelying.63 <20290361+imightbelying63@users.noreply.github.com>
2018-04-14 00:52:47 -0400

    init translate.py

---
+++ scripts/translate.py
@@ -0,0 +1,37 @@
+""" translate input to a coded message where:
+    characters are replaced with the key on the opposite side of the kb
+    examples:
+    
+    * a = 1
+    * g = v
+    * q = p
+
+"""
+
+table = {'a' = 'l'
+        'b' = 'c'
+        'c' = 'b'
+        'd' = 'j'
+        'e' = 'i'
+        'f' = 'h'
+        'g' = 'v'
+        'h' = 'f'
+        'i' = 'e'
+        'j' = 'd'
+        'k' = 's'
+        'l' = 'a'
+        'm' = 'z'
+        'n' = 'x'
+        'o' = 'w'
+        'p' = 'q'
+        'q' = 'p'
+        'r' = 'u'
+        's' = 'k'
+        't' = 'y'
+        'u' = 'r'
+        'v' = 'g'
+        'w' = 'o'
+        'x' = 'n'
+        'y' = 't'
+        'z' = 'm
+        }
