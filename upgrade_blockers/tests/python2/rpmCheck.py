435d64660d2bfb130a45829c05461ffbcfd4f824
imightbelying63 <user.email=20290361+imightbelying63@users.noreply.github.com>
2018-04-14 12:03:54 -0400

    python2 rpmCheck

--- upgrade_blockers/tests/python2/rpmCheck.py
+++ upgrade_blockers/tests/python2/rpmCheck.py
@@ -2,11 +2,18 @@
    I'll use yum; yum working implies a healthy rpm db
 """
 
-if __name__ == "__main__":
-    import subprocess
+import subprocess
 
 def rpmCheck():
-    test_rpm = 'sl' #DO NOT USE IN PRODUCTION, sl IS IN EPEL
+    test_rpm = 'sl' #DO NOT USE IN PRODUCTION, sl IS IN EPEL 
+
+    cmd1 = "yum -y --quiet install " + test_rpm
+    cmd2 = "yum -y --quiet remove " + test_rpm
+
+    rpm1 = subprocess.Popen(cmd1, shell=True, stdout=subprocess.PIPE)
+    rpm2 = subprocess.Popen(cmd2, shell=True, stdout=subprocess.PIPE)
+
+    install = rpm1.communicate
 
     if subprocess.getstatusoutput('yum -y --quiet install sl')[0] == 0:
         if subprocess.getstatusoutput('yum -y --quiet remove sl')[0] == 0:
