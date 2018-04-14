028e0b11d34d1888f57a1103fa4932288c87ec88
imightbelying.63 <20290361+imightbelying63@users.noreply.github.com>
2018-04-14 01:48:28 -0400

    :

--- scripts/translate.py
+++ scripts/translate.py
@@ -8,30 +8,38 @@
 
 """
 
-table = {'a' = 'l'
-        'b' = 'c'
-        'c' = 'b'
-        'd' = 'j'
-        'e' = 'i'
-        'f' = 'h'
-        'g' = 'v'
-        'h' = 'f'
-        'i' = 'e'
-        'j' = 'd'
-        'k' = 's'
-        'l' = 'a'
-        'm' = 'z'
-        'n' = 'x'
-        'o' = 'w'
-        'p' = 'q'
-        'q' = 'p'
-        'r' = 'u'
-        's' = 'k'
-        't' = 'y'
-        'u' = 'r'
-        'v' = 'g'
-        'w' = 'o'
-        'x' = 'n'
-        'y' = 't'
-        'z' = 'm
+table = {'a' : 'l',
+        'b' : 'c',
+        'c' : 'b',
+        'd' : 'j',
+        'e' : 'i',
+        'f' : 'h',
+        'g' : 'v',
+        'h' : 'f',
+        'i' : 'e',
+        'j' : 'd',
+        'k' : 's',
+        'l' : 'a',
+        'm' : 'z',
+        'n' : 'x',
+        'o' : 'w',
+        'p' : 'q',
+        'q' : 'p',
+        'r' : 'u',
+        's' : 'k',
+        't' : 'y',
+        'u' : 'r',
+        'v' : 'g',
+        'w' : 'o',
+        'x' : 'n',
+        'y' : 't',
+        'z' : 'm',
         }
+
+message = str(input("what is your secret message?\n"))
+
+for char in message[:]:
+    if char in table.keys():
+        message = message.replace(char, table[char])
+
+print(message)
