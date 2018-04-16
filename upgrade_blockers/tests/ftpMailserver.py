f5f020daecc5866d5afb097492ad515a38d23454
imightbelying63 <user.email=20290361+imightbelying63@users.noreply.github.com>
2018-04-16 16:43:12 -0400

    init ftpMailServer

--- upgrade_blockers/tests/ftpMailserver.py
+++ upgrade_blockers/tests/ftpMailserver.py
@@ -8,20 +8,22 @@ def ftpMailserver():
     ftp_mailserver = []
 
     conf_file = '/var/cpanel/cpanel.config'
-    ftp_regex = '(ftpserver=)\s*(pure\-ftpd|proftpd)'
-    mail_regex = '(mailserver=)\s*(dovecot|courier)'
+    ftp_regex = '(ftpserver=)\s*(pure\-ftpd|proftpd)$'
+    mail_regex = '(mailserver=)\s*(dovecot|courier)$'
     ftp_is_set = False
     mailserver_is_set = False
     with open(conf_file) as conf:
         for line in conf.readlines():
-            ftp_match = re.search(ftp_regex, line)
-            #valid configs are pure-ftpd or proftpd
-            if ftp_match.groups()[1] == "pure-ftpd" or ftp_match.groups()[1] == "proftpd":
-                ftp_is_set = True
-            mail_match = re.search(mail_regex, line)
-            #valid configs are dovecot or courier
-            if mail_match.groups()[1] == "dovecot" or mail_match.groups()[1] == "courier":
-                mailserver_is_set = True
+            if re.search(ftp_regex, line):
+                #valid configs are pure-ftpd or proftpd
+                ftp_match = re.search(ftp_regex, line)
+                if ftp_match.groups()[1] == "pure-ftpd" or ftp_match.groups()[1] == "proftpd":
+                    ftp_is_set = True
+            if re.search(mail_regex, line):
+                mail_match = re.search(mail_regex, line)
+                #valid configs are dovecot or courier
+                if mail_match.groups()[1] == "dovecot" or mail_match.groups()[1] == "courier":
+                    mailserver_is_set = True
 
     if not ftp_is_set:
         ftp_mailserver.append("The ftpserver value in " + conf_file + " is invalid. Valid values are pure-ftpd or proftpd.")
