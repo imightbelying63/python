0119136e7928ed9827de8da3cabfcd3331797e04
imightbelying63 <user.email=20290361+imightbelying63@users.noreply.github.com>
2018-04-16 17:37:38 -0400

    udated TODOs

--- upgrade_blockers/upgrade_blockers.py
+++ upgrade_blockers/upgrade_blockers.py
@@ -1,18 +1,13 @@
 """This script will check if a cPanel update is available, and if so, are there any blockers that will prevent it from proceeding.
 
-  TODO: ensure run as root (DONE)
-  TODO: verify license file
-  TODO: check write-access to required file systems
-  TODO: ensure rpm integrity
-  TODO: plethora of version specific checks
-
-  This will work around python3 unavailability on cent 4/5 because those are guaranteed blockers anyway
+  There is a version for python2 and python3.  if python3 is an uncertainty
+   default to python2
 
   At present, most code is separated out into single test files stored under tests/
 
   Current exit codes:
    1: non-root
-   2: python or centos version too low
+   2: centos version too low
 
 
 
