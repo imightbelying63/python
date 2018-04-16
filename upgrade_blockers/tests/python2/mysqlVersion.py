6a1d3540f5a817330fce1f5300756f119ff4f539
imightbelying63 <user.email=20290361+imightbelying63@users.noreply.github.com>
2018-04-16 16:29:42 -0400

    python2 tests

--- upgrade_blockers/tests/python2/mysqlVersion.py
+++ upgrade_blockers/tests/python2/mysqlVersion.py
@@ -1,10 +1,10 @@
 import subprocess
 
-def mysqlVersion2():
+def mysqlVersion():
     cmd = "mysqladmin version|grep 'Server version'"
     mysqladmin = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
     mysql_version = mysqladmin.communicate()[0].expandtabs().rstrip().split()[2]
     return float(''.join(mysql_version[:3]))
 
 if __name__ == "__main__":
-    print(mysqlVersion2())
+    print mysqlVersion()
