d925cb4de9a79508d543eadcdfed17fd75b563d5
imightbelying63 <user.email=20290361+imightbelying63@users.noreply.github.com>
2018-04-16 20:53:54 -0400

    added argument

--- upgrade_blockers/tests/argParse.py
+++ upgrade_blockers/tests/argParse.py
@@ -4,8 +4,6 @@ import argparse
 
 parser = argparse.ArgumentParser(description="Iterates over all possible cPanel update blockers, and informs when one is present")
 parser.add_argument("--skip-rpm-check", help="RPM check adds wait time, if you're positive RPM is working, skip this to increase the speed at which this script runs", action="store_true")
+parser.add_argument("--raw", help="Doesn't attempt to fill the customer reply in, simply delivers the output of any given blocker")
 
 args = parser.parse_args()
-
-if args.skip_rpm_check:
-   print('sure')
