678569ec9575c358bd3139aadb461279163eba00
imightbelying63 <20290361+imightbelying63@users.noreply.github.com>
2018-04-26 22:48:44 -0400

    added args

--- utils/pwGen.py
+++ utils/pwGen.py
@@ -1,9 +1,17 @@
 """ generate a psuedo-random password """
 
 from random import *
+import argparse as ap
 
 PASSWORD_LENGTH = 13
 CHARACTERS = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890_!@#$%^&*()+'
+
+#argument handling
+p = ap.ArgumentParser()
+p.add_argument("-l", "--length", type=int, help="set password length. default: "+str(PASSWORD_LENGTH))
+args = p.parse_args()
+if args.length:
+    PASSWORD_LENGTH = args.length if args.length > 0 else PASSWORD_LENGTH
 
 count = 0
 new_password = ''
