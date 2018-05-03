31fa061460c349f735c7dba135fd4ba1122afd57
Keven Hughes <user.email=20290361+imightbelying63@users.noreply.github.com>
2018-05-03 16:32:32 -0400

    update args handling

--- cpanel/muhpanels.py
+++ cpanel/muhpanels.py
@@ -3,13 +3,19 @@
 
    TODO: add pecl handling in EA4
    TODO: show if is ea3 or 4
+
+   This uses argparse in a slightly unconventional way by displaying positional args
+   as script commands, but just parsing the input of the first positional (args.check)
+   in order to control script flow
 """
 import os, sys, subprocess
 import argparse as ap
 
 parser = ap.ArgumentParser()
+parser._positionals.title = "Script Commands"
 parser.add_argument("check", help="invoke cPanel SSP server checking util", nargs='?', default=False)
 parser.add_argument("whm_backdoor", help="generates a root login session to WHM", nargs='?', default=False)
+parser.add_argument("domains", help="extract domains from httpd and report if resolve locally or not", nargs='?', default=False)
 args = parser.parse_args()
 
 
@@ -26,4 +32,34 @@ def whmBackdoor():
     print whmapi.communicate()[0]
     return None
 
-print args.check, args.whm_backdoor
+def domains():
+    RAW_DOMAINS = 'https://raw.githubusercontent.com/imightbelying63/python/master/cpanel/domains2.py'
+    try:
+        import domains2
+    except ImportError:
+        try:
+           urllib.urlretrieve(RAW_DOMAINS, 'domains2.py')
+           import domains2
+           domains2_file = RAW_DOMAINS.split(os.path.sep)[-1]
+           if os.path.exists(domains2_file):
+               os.remove(domains2_file)
+               if os.path.exists(domains2_file + 'c'):
+                   os.remove(domains2_file + 'c')
+        except:
+            print 'unable to retrieve required script'    
+    return None
+
+def main():
+    #work down the list of potential commands
+    if args.check == "check":
+        print 'do check'
+    elif args.check == 'whm_backdoor':
+        print 'do whm_backdoor'
+    elif args.check == 'domains':
+        print 'do domains'
+    else:
+        parser.print_help()
+
+if __name__ == "__main__":
+    main()
+
