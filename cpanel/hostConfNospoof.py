940a31c5b66936ef4e7b7c84102c79380f585a79
Keven Hughes <user.email=20290361+imightbelying63@users.noreply.github.com>
2018-05-22 19:50:56 -0400

    incorrect filename update
    removed old script hostsConfNospoof.py

---
+++ cpanel/hostConfNospoof.py
@@ -0,0 +1,50 @@
+"""
+    This file searches for an old directive in hosts.conf 'nospoof' and removes it
+    if it's found.  otherwise, do nothing
+"""
+
+import re,os
+try:
+    import shutil
+except:
+    pass
+
+HOST_CONF = '/etc/host.conf'
+HOST_CONF_BAK = HOST_CONF+".bak"
+
+def hostConfNospoof():
+    if not os.path.exists(HOST_CONF):
+        print HOST_CONF + " does not exist.  nothing to do"
+        return None
+
+    cleaned_file = False
+    
+    spoof_regex = re.compile('nospoof\s+.+')
+    with open(HOST_CONF) as conf:
+        lines = conf.readlines()
+
+    for line in lines:
+        if spoof_regex.search(line.rstrip()):
+            #if we can, lets make a backup copy
+            try:
+                shutil.copyfile(HOST_CONF, HOST_CONF_BAK)
+            except:
+                pass
+            lines.remove(line)
+            cleaned_file = True
+            break
+
+    with open(HOST_CONF, "w") as conf:
+        for line in lines:
+            conf.write(line)
+
+    if cleaned_file:
+        if os.path.exists(HOST_CONF_BAK):
+            print "backed up original to " + HOST_CONF_BAK 
+        print "Cleaned nospoof from " + HOST_CONF
+    else:
+        print "nospoof not found in " + HOST_CONF + ". nothing to do"
+
+
+if __name__ == "__main__":
+    hostConfNospoof()
